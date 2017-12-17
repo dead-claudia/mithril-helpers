835f1cb06b5385bb6f7fd69e7e6a56eb54dbff6b
alfa <alfa.jpn@gmail.com>
2017-12-17 21:24:46 +0900

    Fix code style.

--- tests/migrate/v2_spec.js
+++ tests/migrate/v2_spec.js
@@ -1,155 +1,155 @@
-const o = require('../spec_helper');
-const v2 = require('../../migrate/v2').v2;
+const o = require("../spec_helper")
+const v2 = require("../../migrate/v2").v2
 
-o.describe('migrate/v2', () => {
-  o.beforeEach(() => {
-    document.body.innerHTML = ''
-  })
-
-  o.describe('v2', () => {
-    const subject = (() => v2('div'))
-
-    o.it('Return vnode', () => {
-      o.expect(subject().tag).equals("div")
-    })
-  })
-
-  o.describe('.render', () => {
-    const subject = (() => v2.render(document.body, v2('span', 'Hello')))
-
-    o.it('Render dom', () => {
-      subject()
-      o.expect(document.body.firstChild.nodeName).equals("SPAN")
-    })
-  })
-
-  o.describe('.mount', () => {
-    const subject = (() => {
-      v2.mount(document.body, {view: () => v2('main', 'Hello')})
+o.describe("migrate/v2", () => {
+    o.beforeEach(() => {
+        document.body.innerHTML = ""
     })
 
-    o.it('Render dom', () => {
-      subject()
-      o.expect(document.body.firstChild.nodeName).equals("MAIN")
-    })
-  })
+    o.describe("v2", () => {
+        const subject = () => v2("div")
 
-  o.describe('#route', () => {
-    const subject = (() => {
-      v2.route(
-        document.body,
-        '/',
-        {
-          '/': {view: () => v2('header', 'Hello')}
+        o.it("Return vnode", () => {
+            o.expect(subject().tag).equals("div")
+        })
+    })
+
+    o.describe(".render", () => {
+        const subject = () => v2.render(document.body, v2("span", "Hello"))
+
+        o.it("Render dom", () => {
+            subject()
+            o.expect(document.body.firstChild.nodeName).equals("SPAN")
+        })
+    })
+
+    o.describe(".mount", () => {
+        const subject = () => {
+            v2.mount(document.body, {view: () => v2("main", "Hello")})
         }
-      )
-    });
 
-    o.it('Render dom', () => {
-      subject()
-      return new Promise((r) => {
-        setTimeout(() => {
-          o.expect(document.body.firstChild.nodeName).equals("HEADER")
-          r();
-        }, 100)
-      })
+        o.it("Render dom", () => {
+            subject()
+            o.expect(document.body.firstChild.nodeName).equals("MAIN")
+        })
     })
-  })
 
-  o.describe('.request', () => {
-    const subject = (() => v2.request('127.0.0.1').catch(() => null))
+    o.describe("#route", () => {
+        const subject = () => {
+            v2.route(
+                document.body,
+                "/",
+                {
+                    "/": {view: () => v2("header", "Hello")}
+                }
+            )
+        }
 
-    o.it('Return promise', () => {
-      o.expect(typeof subject().then === 'function').equals(true)
-      o.expect(typeof subject().catch === 'function').equals(true)
+        o.it("Render dom", () => {
+            subject()
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    o.expect(document.body.firstChild.nodeName).equals("HEADER")
+                    resolve()
+                }, 100)
+            })
+        })
     })
-  })
 
-  o.describe('.jsonp', () => {
-    const subject = (() => v2.jsonp('127.0.0.1').catch(() => null))
+    o.describe(".request", () => {
+        const subject = () => v2.request("127.0.0.1").catch(() => null)
 
-    o.it('Return promise', () => {
-      o.expect(typeof subject().then === 'function').equals(true)
-      o.expect(typeof subject().catch === 'function').equals(true)
+        o.it("Return promise", () => {
+            o.expect(typeof subject().then === "function").equals(true)
+            o.expect(typeof subject().catch === "function").equals(true)
+        })
     })
-  })
 
-  o.describe('.parseQueryString', () => {
-    const subject = (() => v2.parseQueryString(value))
-    let value = null;
+    o.describe(".jsonp", () => {
+        const subject = () => v2.jsonp("127.0.0.1").catch(() => null)
 
-    o.context('When value is array', () => {
-      o.beforeEach(() => {
-        value = 'a[0]=x&a[1]=y'
-      })
-
-      o.it('Parse query string', () => {
-        o.expect(subject().a[0]).equals('x')
-        o.expect(subject().a[1]).equals('y')
-      })
+        o.it("Return promise", () => {
+            o.expect(typeof subject().then === "function").equals(true)
+            o.expect(typeof subject().catch === "function").equals(true)
+        })
     })
-  })
 
-  o.describe('.buildQueryString', () => {
-    const subject = (() => v2.buildQueryString(value))
-    let value = null;
+    o.describe(".parseQueryString", () => {
+        const subject = () => v2.parseQueryString(value)
+        let value = null
 
-    o.context('When value is array', () => {
-      o.beforeEach(() => {
-        value = {a: ['x', 'y']}
-      })
+        o.context("When value is array", () => {
+            o.beforeEach(() => {
+                value = "a[0]=x&a[1]=y"
+            })
 
-      o.it('Parse query string', () => {
-        o.expect(subject()).equals(encodeURI('a[0]=x&a[1]=y'))
-      })
+            o.it("Parse query string", () => {
+                o.expect(subject().a[0]).equals("x")
+                o.expect(subject().a[1]).equals("y")
+            })
+        })
     })
-  })
 
-  o.describe('.withAttr', () => {
-    const subject = (() => v2.withAttr('value', () => null))
+    o.describe(".buildQueryString", () => {
+        const subject = () => v2.buildQueryString(value)
+        let value = null
 
-    o.it('Return function', () => {
-      o.expect(typeof subject() === 'function').equals(true)
+        o.context("When value is array", () => {
+            o.beforeEach(() => {
+                value = {a: ["x", "y"]}
+            })
+
+            o.it("Parse query string", () => {
+                o.expect(subject()).equals(encodeURI("a[0]=x&a[1]=y"))
+            })
+        })
     })
-  })
 
-  o.describe('.trust', () => {
-    const subject = (() => v2.trust('<script>alert(0)</script>'))
+    o.describe(".withAttr", () => {
+        const subject = () => v2.withAttr("value", () => null)
 
-    o.it('Return vnode', () => {
-      o.expect(subject().tag).equals('<')
-      o.expect(subject().children).equals('<script>alert(0)</script>')
+        o.it("Return function", () => {
+            o.expect(typeof subject() === "function").equals(true)
+        })
     })
-  })
 
-  o.describe('.fragment', () => {
-    const subject = (() => v2.fragment({oninit: () => null}, [v2('div')]))
+    o.describe(".trust", () => {
+        const subject = () => v2.trust("<script>alert(0)</script>")
 
-    o.it('Return vnodes', () => {
-      o.expect(subject().tag).equals('[')
-      o.expect(subject().children[0].tag).equals('div')
+        o.it("Return vnode", () => {
+            o.expect(subject().tag).equals("<")
+            o.expect(subject().children).equals("<script>alert(0)</script>")
+        })
     })
-  })
 
-  o.describe('.redraw', () => {
-    const subject = (() => v2.redraw())
+    o.describe(".fragment", () => {
+        const subject = () => v2.fragment({oninit: () => null}, [v2("div")])
 
-    o.it('Not raise error', () => {
-      let error = null;
-      try {
-        subject()
-      } catch (e) {
-        error = e
-      }
-      o.expect(error).equals(null)
+        o.it("Return vnodes", () => {
+            o.expect(subject().tag).equals("[")
+            o.expect(subject().children[0].tag).equals("div")
+        })
     })
-  })
 
-  o.describe('.version', () => {
-    const subject = (() => v2.version)
+    o.describe(".redraw", () => {
+        const subject = () => v2.redraw()
 
-    o.it('Return version', () => {
-      o.expect(subject()).equals('1.0.0.migrate')
+        o.it("Not raise error", () => {
+            let error = null
+            try {
+                subject()
+            } catch (e) {
+                error = e
+            }
+            o.expect(error).equals(null)
+        })
     })
-  })
+
+    o.describe(".version", () => {
+        const subject = () => v2.version
+
+        o.it("Return version", () => {
+            o.expect(subject()).equals("1.0.0.migrate")
+        })
+    })
 })
--- tests/spec_helper.js
+++ tests/spec_helper.js
@@ -1,10 +1,10 @@
-const o = require('mithril/ospec');
-const browserMock = require("mithril/test-utils/browserMock.js");
+const o = require("mithril/ospec")
+const browserMock = require("mithril/test-utils/browserMock.js")
 
-global.window = browserMock();
-global.document = window.document;
+global.window = browserMock()
+global.document = window.document
 
-o.describe = o.context = o.spec;
-o.it = o.expect = o;
+o.describe = o.context = o.spec
+o.it = o.expect = o
 
-module.exports = o;
+module.exports = o
