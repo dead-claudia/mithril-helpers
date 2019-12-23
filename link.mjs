0ef88e07bcf47f54d08357a3500649d16eacab32
Isiah Meadows <contact@isiahmeadows.com>
2019-12-23 00:03:34 -0500

    Create link.mjs

---
+++ link.mjs
@@ -0,0 +1,15 @@
+/**
+ * Use this to easily link to an identity key. Keys are always converted to
+ * properties, so `null`/`undefined` are equivalent to `"null"` and
+ * `"undefined"` respectively.
+ *
+ * This is just a preview of something I'm looking to add in my redesign.
+ */
+
+import Vnode from "mithril/render/vnode"
+
+export default function link(key, ...children) {
+    return Vnode("[", null, null, [
+        Vnode("[", key, null, Vnode.normalizeChildren(children))
+    ])
+}
