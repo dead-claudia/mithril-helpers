66536f01847aaae9888373e220e3eefc8cd40ff0
Isiah Meadows <contact@isiahmeadows.com>
2019-12-23 00:55:28 -0500

    Update each.d.ts

--- each.d.ts
+++ each.d.ts
@@ -2,10 +2,13 @@
 import * as Mithril from "mithril";
 
 /**
- * Build a keyed fragment using separate key + child functions.
+ * Build a keyed fragment using separate key + child pairs.
+ *
+ * If `by` is a property key, it's sugar for passing `v => v[by]`. For `child`,
+ * it's sugar for `v => "" + v[by]` - it's always rendered as a text node.
  */
 export default function each<T>(
     list: ArrayLike<T>,
-    by: (value: T) => PropertyKey,
-    child: (value: T) => Mithril.Vnode<any, any>
+    by: keyof T | ((value: T, index: number) => PropertyKey),
+    child: keyof T | ((value: T, index: number) => Mithril.Vnode<any, any>)
 ): Mithril.Vnode<any, any>
