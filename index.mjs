b781890899fc2bc461d08b89005fb26fd63ab4df
Isiah Meadows <contact@isiahmeadows.com>
2019-12-23 00:37:32 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,9 @@
  * This is a module collecting all the other submodules for easy access.
  */
 
-export {default as censor} from "./censor.js"
-export {default as SelfSufficient} from "./self-sufficient.js"
-export {default as makeStore} from "./store.js"
-export {default as makeRedraw} from "./redraw.js"
+export {default as censor} from "./censor.mjs"
+export {default as SelfSufficient} from "./self-sufficient.mjs"
+export {default as makeStore} from "./store.mjs"
+export {default as makeRedraw} from "./redraw.mjs"
+export {default as each} from "./each.mjs"
+export {default as link} from "./link.mjs"
