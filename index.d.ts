abda655b8eb2f33d652bc23d157bc8ee9ab2f4e3
Isiah Meadows <contact@isiahmeadows.com>
2019-12-23 02:27:12 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,9 @@
 /// <reference types="mithril" />
 import * as Mithril from "mithril";
 
-export {default as censor} from "./censor.d";
-export {default as makeStore, Store, OnChange} from "./store.d";
-export {default as makeRedraw, Redraw, RedrawState} from "./redraw.d";
-export {default as SelfSufficient, MithrilEvent, SelfSufficientParams, SelfSufficientState} from "./self-sufficient.d";
+export {default as censor} from "./censor";
+export {default as makeStore, Store, OnChange} from "./store";
+export {default as makeRedraw, Redraw, RedrawState} from "./redraw";
+export {default as SelfSufficient, MithrilEvent, SelfSufficientParams, SelfSufficientState} from "./self-sufficient";
+export {default as each} from "./each"
+export {default as link} from "./link"
